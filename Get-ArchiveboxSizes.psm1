e1c5b99e39093ce06c3d155532a86e31291a89bb
RoyApalnes <roy.apalnes@gmail.com>
2015-10-08 11:08:39 +0200

    New Scripts
    Export Mailbox and Archive Sizes to CSV with DisplayName.

---
+++ Get-ArchiveboxSizes.psm1
@@ -0,0 +1,31 @@
+﻿Function Get-ArchiveMailboxSizes
+
+{
+    [cmdletbinding()]
+    Param
+    (
+    
+    )
+    Begin
+    {
+        #Commands we run one time
+        #Collect all Mailbox Objects
+        $MailboxUsers = Get-Mailbox -ResultSize Unlimited
+    }
+    Process
+    {
+         #Collect DisplayName and TotalItemSize for each $Mailbox, defined by UPN, in $MailboxUsers
+         $Stats = foreach($Mailbox in $MailboxUsers)
+        {
+            #
+            Get-MailboxStatistics -Identity $Mailbox.UserPrincipalName -Archive | Select-Object DisplayName,TotalItemSize
+        }
+
+        #Export Data to CSV
+        $Stats | Export-Csv D:\Temp\Product\ReportArchive.csv
+    }
+    End
+    {
+        #Commands we run when Script is ending
+    }
+} 
---
+++ Get-MailboxSizes.psm1
@@ -0,0 +1,32 @@
+﻿Function Get-MailboxSizes
+
+{
+    [cmdletbinding()]
+    Param
+    (
+    
+    )
+    Begin
+    {
+        #Commands we run one time
+
+        #Collect all Mailbox Objects
+        $MailboxUsers = Get-Mailbox -ResultSize Unlimited
+    }
+    Process
+    {
+         #Collect DisplayName and TotalItemSize for each $Mailbox, defined by UPN, in $MailboxUsers.
+         $Stats = foreach($Mailbox in $MailboxUsers)
+        {
+            #Get DisplayName and TotalItemSize
+            Get-MailboxStatistics -Identity $Mailbox.UserPrincipalName | Select-Object DisplayName,TotalItemSize
+        }
+        
+        #Export Data to CSV
+        $Stats | Export-Csv D:\Temp\Product\Report3.csv
+    }
+    End
+    {
+        #Commands we run when Script is ending
+    }
+} 
