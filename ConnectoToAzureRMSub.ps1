8d99b35a670ed877f4626b0cedf7b6185c1fbaa2
Roy Apalnes <roy.apalnes@gmail.com>
2016-10-01 04:53:54 +0200

    Add files via upload

---
+++ ConnectoToAzureRMSub.ps1
@@ -0,0 +1,19 @@
+﻿#Get connected to your Azure Subscription.
+
+#Install Azure RM Commands.
+Install-Module AzureRM
+
+#Import Azure RM Commands.
+Import-Module AzureRM
+
+#Login to AzureRM.
+Login-AzureRmAccount
+
+#See which Azure RM Subscription your currently managin.
+Get-AzureRmContext
+
+#Get a view of all Azure RM Subscriptions your account is administrating.
+Get-AzureRmSubscription
+
+#Select a subscription by copying the SubscriptionName.
+Get-AzureRmSubscription –SubscriptionName "MSDN-plattformer" | Select-AzureRmSubscription
